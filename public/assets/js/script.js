bc8bf215bf5a0c34184e2cb3c45101d6b9ce4f04
Binjuhor <binjuhor@gmail.com>
2017-10-18 01:54:56 +0700

    Update style and contact content

---
+++ public/assets/js/script.js
@@ -0,0 +1,6 @@
+(function($){
+  "use strict";
+  $('button').click(function(event) {
+    alert("Contact me: \nEmail: binjuhor@gmail.com\nSkype: living_to_do\nThank for your intresting.");
+  });
+})(jQuery)
