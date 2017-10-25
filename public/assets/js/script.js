d1ba5e850428928d795c76c8dd64a10d4d2a1ae1
Binjuhor <binjuhor@gmail.com>
2017-10-26 02:52:42 +0700

    Update social information

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,6 +1,6 @@
 (function($){
   "use strict";
   $('button').click(function(event) {
-    alert("Contact me: \nEmail: binjuhor@gmail.com\nSkype: living_to_do\nThank for your intresting.");
+    alert("Contact me: \nEmail: binjuhor@gmail.com\nSkype: living_to_do\nThank for your interested.");
   });
 })(jQuery)
