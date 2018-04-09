5f7109d9cccbd39416d188165e4335e48d3e41db
Binjuhor <binjuhor@gmail.com>
2018-04-09 21:32:35 +0700

    v1.0.0 + Re upload js to combile.

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,6 +1,8 @@
 (function($){
   "use strict";
+  
   $('button').click(function(event) {
     alert("Contact me: \nEmail: binjuhor@gmail.com\nSkype: living_to_do\nThank for your interested.");
   });
+
 })(jQuery)
